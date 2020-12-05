f7741378f12f564b5c1b11e2b2a8eb10dacdfcf2
Myles Carey <seanmyles@comcast.net>
2020-12-05 11:58:57 -0800

    this is a commit multi-line for a maven course exercise

---
+++ src/main/java/com/example/controllers/ExampleController.java
@@ -0,0 +1,18 @@
+package com.example.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleController {
+  @RequestMapping("/")
+   public Map<String, Object> getIndex() {
+    Map<String, Object> returnval = new HashMap<>();
+
+    returnval.put("hello","world");
+    return returnval;
+  }
+}
